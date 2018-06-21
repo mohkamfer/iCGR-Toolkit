c416c92c7edfb66271a71a826679dbd7b46937bb
mohkamfer <mohammedkamalfergany@yahoo.com>
2018-06-21 22:02:00 +0200

    Add basic layout structure

--- app/src/main/java/com/mohkamfer/icgrtoolkit/fragment/GraphFragment.java
+++ app/src/main/java/com/mohkamfer/icgrtoolkit/fragment/GraphFragment.java
@@ -1,6 +1,20 @@
 package com.mohkamfer.icgrtoolkit.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mohkamfer.icgrtoolkit.R;
 
 public class GraphFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_graph, container, false);
+        return view;
+    }
 }
